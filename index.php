280ff963cc172cfa07209943f92ce54b195bed16
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-01-30 23:24:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div class="header-main-container">
+        <!-- <div class="header-name-container"> -->
+          <div class="header-name">
+            <h2>Contacts</h2>
+          </div>
+        <!-- </div> -->
+        <!-- <div class="header-links-container"> -->
+          <div class="header-links">
+            <a href="#">Sign In</a>
+          </div>
+        <!-- </div> -->
+      </div>
+    </header>
+  </body>
+</html>
