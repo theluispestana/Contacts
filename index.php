f59bcb9065bbcc306f71531bf61d1c81e4f153ff
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-02-02 01:38:16 -0500

    improvements

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
   include_once 'header.php';
 ?>
 
-    <div class="content-container">
-      <div class="content-text">
-        <h1>Contacts test</h1>
+    <div class="content-main-container">
+      <div class="content-text-container">
+          <h1>Contacts test</h1>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum felis vitae suscipit efficitur. Duis ut metus non diam scelerisque aliquam. Aenean fermentum sem sed orci hendrerit, ut molestie magna blandit. Sed hendrerit feugiat mi, et hendrerit est. Donec vel tortor in tellus cursus posuere. Maecenas fermentum est urna, et condimentum lorem vestibulum vitae. Pellentesque eu aliquam nunc. Duis vel turpi</p>
       </div>
       <div class="form-container">
         <form class="sign-up" action="" method="post">
