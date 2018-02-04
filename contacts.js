2ab1d3c16f70eb5d3448cd5e873800a2f2c7a215
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-02-04 01:02:44 -0500

    fixed javascript function

--- contacts.js
+++ contacts.js
@@ -1,22 +1,21 @@
 let signInText = document.querySelectorAll('.header-links');
 let form = document.querySelectorAll('.sign');
 
-function toggleform() {
+function toggleForm() {
   if (form[1].classList.contains('hide')) {
     form[0].classList.add('hide');
     form[1].classList.remove('hide');
-    signInText[0].classList.add('hide');
-    signInText[1].classList.remove('hide');
+    signInText[0].children[0].classList.add('hide');
+    signInText[0].children[1].classList.remove('hide');
   }
   else {
     form[0].classList.remove('hide');
     form[1].classList.add('hide');
-    signInText[0].classList.remove('hide');
-    signInText[1].classList.add('hide');
+    signInText[0].children[0].classList.remove('hide');
+    signInText[0].children[1].classList.add('hide');
   }
 }
 
-
-for (var i = 0; i < signInText.length; i++) {
-  signInText[i].addEventListener('click', toggleform);
+for (var i = 0; i < signInText[0].children.length; i++) {
+  signInText[0].children[i].addEventListener('click', toggleForm)
 }
