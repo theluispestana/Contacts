8e43228dfe5e2e1efc6ac54edf7340a9b6edd827
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-02-09 01:26:38 -0500

    saving page to local storage

--- contacts.js
+++ contacts.js
@@ -1,6 +1,13 @@
 let signInText = document.querySelectorAll('.header-links');
 let form = document.querySelectorAll('.sign');
 
+if (form[1].classList != sessionStorage.getItem('form1Cl')) {
+  form[0].classList = sessionStorage.getItem('form0Cl');
+  form[1].classList = sessionStorage.getItem('form1Cl');
+  signInText[0].children[0].classList = sessionStorage.getItem('signin0Cl');
+  signInText[0].children[1].classList = sessionStorage.getItem('signin1Cl');
+}
+
 function toggleForm() {
   if (form[1].classList.contains('hide')) {
     form[0].classList.add('hide');
@@ -16,6 +23,16 @@ function toggleForm() {
   }
 }
 
+function saveClass() {
+  sessionStorage.setItem('form0Cl', form[0].classList);
+  sessionStorage.setItem('form1Cl', form[1].classList);
+  sessionStorage.setItem('signin0Cl', signInText[0].children[0].classList);
+  sessionStorage.setItem('signin1Cl', signInText[0].children[1].classList);
+}
+
 for (var i = 0; i < signInText[0].children.length; i++) {
-  signInText[0].children[i].addEventListener('click', toggleForm)
+  signInText[0].children[i].addEventListener('click', function() {
+    toggleForm();
+    saveClass();
+  });
 }
