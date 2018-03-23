b40e8132512cf8417eb0f8811fc8a65f3e0e95f9
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-03-23 01:17:11 -0400

    made changes to make site compatible with host

--- includes/dbconnect.inc.php
+++ includes/dbconnect.inc.php
@@ -1,12 +1,16 @@
 <?php
 
 $dbServername = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '123456';
-$dbName = 'Contacts';
+$dbUsername = 'onlin402_admin';
+$dbPassword = 'Avoidingtheerror1324';
+$dbName = 'onlin402_Contacts';
 
-try {
-  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-} catch (Exception $e) {
-  echo "Message: ".$e->getMessage();
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
+echo "success";
